08d2024b4585b92f7d1df579950d1ca493522e44
chaoli <chaoleeli@gvt861.com>
2023-10-07 14:33:06 +0800

    初始化

---
+++ rebase-common/src/main/java/com/relengxing/base/exception/ServiceException.java
@@ -0,0 +1,14 @@
+package com.relengxing.base.exception;
+
+/**
+ * @author chaoli
+ * @date 2023-09-24 12:12
+ * @Description
+ **/
+public class ServiceException extends RuntimeException{
+
+    private String code;
+
+
+
+}
---
+++ rebase-common/src/main/java/com/relengxing/base/model/Result.java
@@ -0,0 +1,74 @@
+package com.relengxing.base.model;
+
+/**
+ * @author chaoli
+ * @date 2023-09-24 12:12
+ * @Description
+ **/
+public class Result<T> {
+
+    /**
+     * 自定义业务码
+     */
+    private String code;
+
+    /**
+     * 自定义业务提示说明
+     */
+    private String msg;
+
+    /**
+     * 自定义返回 数据结果集
+     */
+    private T data;
+
+
+    public static <T> Result<T> build(String stateCode, String message, T body) {
+        Result<T> result  = new Result<>();
+        result.setCode(stateCode);
+        result.setMsg(message);
+        result.setData(body);
+        return result;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Result<T> setCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+
+    public Result<T> setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Result<T> setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                ", code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+
+
+
+}
